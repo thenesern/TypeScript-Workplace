a6e2ba821a3409ede430dc0ea45cfbf10002a9b6
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-02-28 20:24:13 +0300

    Created the "ConcatsForm" Logic

--- src/components/ContactsApp/ContactsForm/ContactsForm.tsx
+++ src/components/ContactsApp/ContactsForm/ContactsForm.tsx
@@ -1,26 +1,67 @@
 import styles from "./ContactsForm.module.css";
+import { useRef } from "react";
 
-const ContacsForm = () => {
+const ContacsForm: React.FC<{
+  onAddContact: (
+    firstName: string,
+    lastName: string,
+    phoneNumber: number
+  ) => void;
+}> = (props) => {
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const phoneNumberRef = useRef<HTMLInputElement>(null);
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const firstName = firstNameRef.current!.value;
+    const lastName = lastNameRef.current!.value;
+    const phoneNumber = phoneNumberRef.current!.value;
+
+    props.onAddContact(firstName, lastName, Number(phoneNumber));
+    firstNameRef.current!.value = "";
+    lastNameRef.current!.value = "";
+    phoneNumberRef.current!.value = "";
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <ul className={styles.list}>
         <li className={styles.li}>
           <label className={styles.labels} htmlFor="firstName">
             First Name
           </label>
-          <input className={styles.inputs} type="text" id="firstName" />
+          <input
+            className={styles.inputs}
+            type="text"
+            id="firstName"
+            ref={firstNameRef}
+            required
+          />
         </li>
         <li className={styles.li}>
           <label className={styles.labels} htmlFor="lastName">
             Last Name
           </label>
-          <input className={styles.inputs} type="text" id="lastName" />
+          <input
+            className={styles.inputs}
+            type="text"
+            id="lastName"
+            ref={lastNameRef}
+            required
+          />
         </li>
         <li className={styles.li}>
           <label className={styles.labels} htmlFor="phoneNumber">
             Phone Number
           </label>
-          <input className={styles.inputs} type="number" />
+          <input
+            className={styles.inputs}
+            type="number"
+            id="phoneNumber"
+            ref={phoneNumberRef}
+            required
+          />
         </li>
         <li>
           <button className={styles.button}>Add</button>

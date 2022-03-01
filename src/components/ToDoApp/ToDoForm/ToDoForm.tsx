9563ee4e4c35ab4f09cc81d74cd50e378a4292ff
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-03-01 21:23:20 +0300

    Created "ToDoForm"

---
+++ src/components/ToDoApp/ToDoForm/ToDoForm.tsx
@@ -0,0 +1,28 @@
+import styles from "./ToDoForm.module.css";
+import React, { useRef } from "react";
+
+const ToDoForm: React.FC<{ onAddToDo: (inputValue: string) => void }> = (
+  props
+) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const inputValue = inputRef.current!.value;
+    props.onAddToDo(inputValue);
+    inputRef.current!.value = "";
+  };
+
+  return (
+    <form className={styles.form} onSubmit={submitHandler}>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Add a to do"
+        ref={inputRef}
+      />
+    </form>
+  );
+};
+
+export default ToDoForm;
